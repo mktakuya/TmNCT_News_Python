70087d29655e6ea1730b9efcbbef3b4801eed139
mktakuya <takuya.mk96@gmail.com>
2014-03-30 21:30:11 +0900

    settingsの例を更新

--- example.settings.py
+++ example.settings.py
@@ -1,7 +1,11 @@
 # -*- coding:utf-8 -*-
 
-CONSUMER_KEY = 'your consumer_key'
-CONSUMER_SECRET = 'your consumer_secret'
-ACCESS_TOKEN = 'your access_token'
-ACCESS_TOKEN_SECRET = 'your access_token_secret'
+FEED_URL = 'http://www2.tomakomai-ct.ac.jp/feed' #苫小牧高専のサイトのRSSフィードのURL
+
+DB_NAME = './sqlite3.db' # SQLite3のデータベースファイル保存先
+
+CONSUMER_KEY = 'Twitter Consumer Key'
+CONSUMER_SECRET = 'Twitter Consumer Secret'
+ACCESS_TOKEN = 'Twitter Access Token'
+ACCESS_TOKEN_SECRET = 'Twitter Access Token Secret'
 
