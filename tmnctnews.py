e13ef7cf123b3a7159e35539813b49de30bacdef
mktakuya <takuya.mk96@gmail.com>
2014-03-30 21:29:57 +0900

    とりあえずの完成

---
+++ tmnctnews.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding:utf-8
+import sys
+import tweepy
+import settings
+from modules.newsgetter import NewsGetter
+from modules.dbconnecter import DBConnecter
+from modules.botcore import BotCore
+
+if __name__ == '__main__':
+    newsgetter = NewsGetter(settings.FEED_URL)
+    latest_entry_title = newsgetter.get_latest_entry_title()
+    latest_entry_link = newsgetter.get_latest_entry_link()
+    latest_entry_pubdate = newsgetter.get_latest_entry_pubdate()
+
+    auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
+    auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
+    bot = BotCore(auth)
+
+    dbconnecter = DBConnecter(settings.DB_NAME)
+    dbconnecter.connect_db()
+    dbconnecter.create_cursor()
+    try:
+        dbconnecter.create_entries_table()
+    except:
+        pass
+    
+    try:
+        latest_localdata_pubdate = dbconnecter.get_latest_entry_pub_date()
+    except:
+        pass
+    
+    if not latest_localdata_pubdate:
+        dbconnecter.insert_entry(latest_entry_title, latest_entry_link, latest_entry_pubdate)
+        bot.send_tweet(u"%s\n%s #苫小牧高専" % (latest_entry_title, latest_entry_link))
+        sys.exit()
+
+    if latest_entry_pubdate > latest_localdata_pubdate:
+        dbconnecter.insert_entry(latest_entry_title, latest_entry_link, latest_entry_pubdate)
+        bot.send_tweet(u"%s\n%s #苫小牧高専" % (latest_entry_title, latest_entry_link))
+
